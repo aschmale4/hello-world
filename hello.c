9f955b5501fe74ced2a7893387ef36cd38f9bcdc
aschmale4 <42650683+aschmale4@users.noreply.github.com>
2018-08-24 11:21:57 -0500

    we added hello.c

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Navya and Austin
+ * Date: 2016/11/02
+ *
+ * A simple hello world program in C
+@ -10,7 +10,8 @@
+**/
+int main(int argc, char **argv) {
+
+  printf("I am Navya Singh and my major is Computer Science.\n");
+  printf("My name is Austin Schmale and my major is Software Engineering.\n");
+
+  return 0;
+}
+}
